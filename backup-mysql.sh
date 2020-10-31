9b520f75a27faffbc7f4f8f46debd6e876fea3b1
MaurickThom <thomtwd@gmail.com>
2020-10-31 05:03:53 -0500

    documentando

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -3,15 +3,22 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-set -e
+set -e # detendrá la ejecución del script por si hay alguna falla
 
+
+# variables de solo lectura
+# Estas son como variables globales, es decir, pueden llamarse aún estando dentro de
+#  funciones.
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SCRIPT_NAME="$(basename "$0")"
 
-function assert_is_installed {
+function assert_is_installed { # esta funcion solo se encargara de preguntar si estan instalado los paquetes
 	local readonly name="$1"
-	if [[ ! $(command -v ${name})]]; then
-		log_error "The binary '$name' is required but it isn't in 		our system"
+	if [[ ! $(command -v ${name})]]; then # esto es para validad si el comando no da una respuensta, eso significa que no esta instalado lo cual ejecutara otra funcion con un error
+		log_error "The binary '$name' is required but it isn't in our system"
+
+    # Exit code 0        Success
+    # Exit code 1        General errors
 		exit 1
 }
 
